e830443d2524a3b7401dbfac274de5a46dbe3625
NikolinaTomic <tomic.ninja@gmail.com>
2020-01-18 01:15:51 +0100

    Distribuiran PDAJ projekat

---
+++ Distribuirani proj/pdaj/tasks/server.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+import os
+import socket
+import subprocess
+import time
+import csv
+
+import beam_integrals as bi
+from beam_integrals.beam_types import BaseBeamType
+from beam_integrals.integrals import BaseIntegral
+from celery import chain, chord
+from celery.exceptions import Reject
+import numpy as np
+import tables as tb
+
+import pdaj as ebi
+from ..app import app
+from .worker import gen_simulation_model_params, solve
+
+DEFAULT_RESOLUTION = 60
+DEFAULT_TMAX = 30
+DEFAULT_DT = 0.01
+
+DEFAULT_L1 = 1
+DEFAULT_L2 = 1
+DEFAULT_M1 = 1
+DEFAULT_M2 = 1
+
+@app.task
+def write_csv(results):
+    with open("/results/dist60.csv", 'w') as csvfile:
+        fieldnames = ['theta1_init', 'theta2_init', 'theta1', 'theta2']
+        spamwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        spamwriter.writeheader()
+        for result in results:
+            spamwriter.writerow({'theta1_init': result[0], 'theta2_init': result[1],
+                                 'theta1': result[2][-1], 'theta2': result[3][-1]})
+
+
+## Seeding the computations
+#docker exec -it pdaj_server_1 cat /var/log/supervisor/celery.log
+@app.task
+def seed_computations(ignore_result=True):
+    #if os.path.exists(get_experiment_status_filename('started')):
+    #    raise Reject('Computations have already been seeded!')
+
+    #record_experiment_status.si('started').delay()
+    chord(
+        (solve.s(DEFAULT_L1, DEFAULT_L2, DEFAULT_M1, DEFAULT_M2, DEFAULT_TMAX, DEFAULT_DT, theta1_init, theta2_init)
+        for theta1_init, theta2_init in gen_simulation_model_params(DEFAULT_RESOLUTION)),write_csv.s()).delay()
+
+   # record_experiment_status.si('completed').delay()
+
+## Recording the experiment status
+
+def get_experiment_status_filename(status):
+    return os.path.join(app.conf.STATUS_DIR, status)
+
+def get_experiment_status_time():
+    """Get the current local date and time, in ISO 8601 format (microseconds and TZ removed)"""
+    return datetime.now().replace(microsecond=0).isoformat()
+
+@app.task
+def record_experiment_status(status):
+    with open(get_experiment_status_filename(status), 'w') as fp:
+        fp.write(get_experiment_status_time() + '\n')
---
+++ Distribuirani proj/pdaj/tasks/worker.py
@@ -0,0 +1,51 @@
+from math import log
+
+from beam_integrals.beam_types import BaseBeamType 
+from beam_integrals.exceptions import UnableToGuessScaleFunctionError
+from beam_integrals.integrals import BaseIntegral, integrate
+import numpy as np
+from scipy.integrate import odeint
+from ..app import app
+
+
+SCALE_FACTOR_HELPER = 2.
+g = 9.81
+
+def deriv(y, t, L1, L2, m1, m2):
+    """Return the first derivatives of y = theta1, z1, theta2, z2."""
+    theta1, z1, theta2, z2 = y
+
+    c, s = np.cos(theta1 - theta2), np.sin(theta1 - theta2)
+
+    theta1dot = z1
+    z1dot = (m2 * g * np.sin(theta2) * c - m2 * s * (L1 * z1 ** 2 * c + L2 * z2 ** 2) -
+             (m1 + m2) * g * np.sin(theta1)) / L1 / (m1 + m2 * s ** 2)
+    theta2dot = z2
+    z2dot = ((m1 + m2) * (L1 * z1 ** 2 * s - g * np.sin(theta2) + g * np.sin(theta1) * c) +
+             m2 * L2 * z2 ** 2 * s * c) / L2 / (m1 + m2 * s ** 2)
+    return theta1dot, z1dot, theta2dot, z2dot
+
+@app.task
+def solve(L1, L2, m1, m2, tmax, dt, theta1_init, theta2_init):
+    y0 = np.array([theta1_init, 0, theta2_init, 0])
+    t = np.arange(0, tmax + dt, dt)
+
+    # Do the numerical integration of the equations of motion
+    y = odeint(deriv, y0, t, args=(L1, L2, m1, m2))
+    theta1, theta2 = y[:, 0], y[:, 2]
+
+    # Convert to Cartesian coordinates of the two bob positions.
+    x1 = L1 * np.sin(theta1)
+    y1 = -L1 * np.cos(theta1)
+    x2 = x1 + L2 * np.sin(theta2)
+    y2 = y1 - L2 * np.cos(theta2)
+
+    return theta1_init, theta2_init, theta1, theta2, x1, y1, x2, y2
+
+@app.task
+def gen_simulation_model_params(theta_resolution):
+    search_space = np.linspace(0, 2 * np.pi, theta_resolution)
+    for theta1_init in search_space:
+        for theta2_init in search_space:
+            yield theta1_init, theta2_init
+
